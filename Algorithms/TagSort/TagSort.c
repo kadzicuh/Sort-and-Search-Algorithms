17ea8ed64ddd0daf4e51e8f3137a33bf1f8a32e1
kadzicu <kadzicuh@outlook.com>
2022-03-03 15:05:18 +0300

    update

--- Algorithms/TagSort/TagSort.c
+++ Algorithms/TagSort/TagSort.c
@@ -26,19 +26,19 @@ SOFTWARE.
 
 void printTag(int arr[], int tag[], const int size)
 {
-	for (int i = 0; i < size; i++)
-		printf("%d ", arr[tag[i]]);
-	printf("\n");
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[tag[i]]);
+    printf("\n");
 }
 
 void tagSort(int arr[], int tag[], const int size)
 {
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = i + 1; j < size; j++)
-		{
-			if (arr[tag[i]] > arr[tag[j]])
-				swap(&tag[i], &tag[j]);
-		}
-	}
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[tag[i]] > arr[tag[j]])
+                swap(&tag[i], &tag[j]);
+        }
+    }
 }
